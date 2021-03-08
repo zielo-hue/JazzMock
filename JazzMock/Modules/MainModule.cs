02055dc40d7bf9f2476ccca9645f64f34a94243f
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-03-08 09:04:18 -0800

    Disqord bot

--- JazzMock/Modules/MainModule.cs
+++ JazzMock/Modules/MainModule.cs
@@ -1,4 +1,23 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Qmmands;
+
+namespace JazzMock.Modules
 {
-  public class $CLASS$ {$END$}
+    public class MainModule : DiscordModuleBase
+    {
+        public DiscordBot Bot { get; set; }
+
+        [Command("help", "info")]
+        [Description("Displays information about the bot.")]
+        public Task HelpAsync()
+            => ReplyAsync(embed: new LocalEmbedBuilder()
+                .WithTitle("jazzmom")
+                .WithDescription("powere b gpt-2 n tensorflow.NET....")
+                .WithColor(Color.Purple)
+                .Build());
+        
+        
+    }
 }
--- JazzMock/Program.cs
+++ JazzMock/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Disqord.Bot.Prefixes;
+using Disqord.Bot.Sharding;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using Qmmands;
 
 namespace JazzMock
 {
-    class Program
+    internal class Program : DiscordBot
     {
-        static void Main(string[] args)
+        private static readonly JObject Conf = JObject.Parse(File.ReadAllText(@"config.json"));
+        private static readonly string BotToken = (string) Conf["DISCORD"]?["TOKEN"];
+
+        private static void Main()
+            => new Program().Run();
+
+        private Program() : base(TokenType.Bot, BotToken,
+            new DefaultPrefixProvider()
+                .AddPrefix("jazzis"),
+            new DiscordBotConfiguration()
+            {
+                Status = UserStatus.Online,
+                ProviderFactory = bot =>
+                    new ServiceCollection()
+                        .AddSingleton((DiscordBot) bot)
+                        .BuildServiceProvider(),
+            })
         {
-            Console.WriteLine("Hello World!");
+            // Disqord seems to log by default now
+            // Logger.Logged += MessageLogged;
+
+            CommandExecutionFailed += Handler;
+
+            AddModules(typeof(Program).Assembly);
+        }
+
+        private void MessageLogged(object sender, Disqord.Logging.LogEventArgs e)
+            => Console.WriteLine(e);
+        
+        // In case of practical problem:
+        private Task Handler(CommandExecutionFailedEventArgs args)
+        {
+            Console.WriteLine(args.Result.Exception.ToString());
+            throw args.Result.Exception;
         }
     }
 }
