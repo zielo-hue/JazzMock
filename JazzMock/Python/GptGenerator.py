70e106a0013116cbc7fa3a62633ffc8f2ff6b510
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-04-07 01:19:20 -0700

    ⚗ Pythonic additions

---
+++ JazzMock/Python/GptGenerator.py
@@ -0,0 +1,15 @@
+﻿import gpt_2_simple as gpt2
+import zmq
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5556")
+
+while True:
+    message = socket.recv()
+    print(f"received request {message}")
+
+    time.sleep(1)
+
+    socket.send(b"World")
