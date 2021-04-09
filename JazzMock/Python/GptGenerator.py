c008be302dcda597388e3c8584283e4841419981
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-04-08 23:17:05 -0700

    GPT-2 ctf_train and cp_generator

--- JazzMock/Python/GptGenerator.py
+++ JazzMock/Python/GptGenerator.py
@@ -2,18 +2,25 @@
 import zmq
 import time
 
+RUN_NAME = "test"
+
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, run_name=RUN_NAME)  # The name of your checkpoint
+graph = gpt2.tf.compat.v1.get_default_graph()
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5556")
 
 while True:
+    print("waiting...")
     message = socket.recv()
     print(f"received request {message}")
 
-    if "die" in str(message):
-        print("got die command, dying...")
-        break
+    results = gpt2.generate(sess, run_name=RUN_NAME, temperature=.9, nsamples=1, batch_size=1,
+                            prefix=message, length=250,
+                            return_as_list=False, include_prefix=False, truncate="\n\n")
 
-    time.sleep(1)
-
-    socket.send(b"World")
+    socket.send(results.encode('utf-8'))
+    print("sent response\n")
+    time.sleep(.1)
--- JazzMock/Python/train.py
+++ JazzMock/Python/train.py
@@ -1,13 +1,15 @@
-﻿import gpt_2_simple as gpt2
+﻿import os
+
+import gpt_2_simple as gpt2
 import sys
 from datetime import datetime
 
-if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+model_name = "355M"
+if not os.path.isdir(os.path.join("models", model_name)):
+    print(f"Downloading {model_name} model...")
+    gpt2.download_gpt2(model_name=model_name)
 
-file_name = "dataset.txt"  # File name of dataset
+file_name = "dataset.csv"  # File name of dataset
 
 sess = gpt2.start_tf_sess()
 
@@ -17,10 +19,10 @@ gpt2.finetune(
     model_name='355M',  # Model you have already downloaded
     steps=-1,  # -1 will do unlimited. Enter number of iterations otherwise
     restore_from='latest',  # Also allows 'fresh' which will overwrite old training
-    run_name='discord',  # The name to pull or create a checkpoint under
+    run_name='test',  # The name to pull or create a checkpoint under
     print_every=50,  # Print iterations every X numebr
     sample_every=150,  # Generate a text sample ever X number of iter.
-    save_every=500,  # Save a snapshot every X number of iter.
+    save_every=100,  # Save a snapshot every X number of iter.
     learning_rate=0.0001,  # Lower to 0.00001 if you are not getting massive changes in results
     batch_size=1  # Keep at 1 or 2, will use up more memory if you raise this
 )
