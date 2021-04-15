b16fc7eafc13bd1a92b8bef620aca4c8d1489361
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-04-14 18:00:47 -0700

    multipart messages

--- JazzMock/Python/GptGenerator.py
+++ JazzMock/Python/GptGenerator.py
@@ -17,15 +17,21 @@ newline = "\n"
 requestCount = 0
 
 while True:
-    print("waiting...")
-    message = socket.recv()
+    print(f"waiting... request count at {requestCount}")
+    multi_message = socket.recv_multipart()
+    args = int.from_bytes(multi_message[0], "big")
+    message = multi_message[1]
+    truncateArg = "<|endoftext|>"
     print(f"received request {message.decode('utf-8')}")
 
     requestCount += 1
+    print(args)
+    if args == 1: # from the genconvo command
+        truncateArg = ""
     results = gpt2.generate(sess, run_name=RUN_NAME, temperature=.7, nsamples=2, batch_size=2,
                             prefix=message.decode(
                                 "utf-8"), length=250,
-                            return_as_list=True, include_prefix=False, truncate="<|endoftext|>")
+                            return_as_list=True, include_prefix=False, truncate=truncateArg)
     for r in results:
         if r.isspace() or len(r.strip()) == 0:
             results.remove(r)
@@ -38,7 +44,7 @@ while True:
 
     print("sent response\n")
 
-    if requestCount > 30:
+    if requestCount > 20:
         print("resetting graph...")
         requestCount = 0
         gpt2.reset_session(sess)
--- JazzMock/Services/GptGeneratorService.cs
+++ JazzMock/Services/GptGeneratorService.cs
@@ -26,6 +26,7 @@ namespace JazzMock.Services
         private RequestSocket _tunnel;
         private Boolean _busySocket;
         private DiscordClientBase _client;
+        private uint _queue;
         
         public GptGeneratorService(ILogger<GptGeneratorService> logger, DiscordClientBase client) : base(logger, client)
         {
@@ -41,24 +42,25 @@ namespace JazzMock.Services
             _rand = new Random();
             _tunnel = new RequestSocket();
             _tunnel.Connect("tcp://127.0.0.1:5556");
+            _queue = 0;
         }
 
         private async Task OnMessage(object sender, MessageReceivedEventArgs eventArgs)
         {
-            var fun = false;
+            var fun = false; // fun value is used to determine whether the bot should answer unprovoked or not
             if (!(eventArgs.Message.Author.Id != _client.CurrentUser.Id
                   && (eventArgs.Message.Content.ToLower().Contains("nemu")
                       || eventArgs.Message.Content.ToLower().Contains(_client.CurrentUser.Name)
                       || eventArgs.Message.MentionedUsers.Contains(_client.CurrentUser))
                   )
-                )
+                ) // check that the message has trigger words or if the message is by the bot itself. if not, continue on
             {
                 fun = _rand.Next(1, 10) == 1;
                 if (!(fun
                       && eventArgs.Message.Author.Id != _client.CurrentUser.Id
                       && (eventArgs.Message.ChannelId == 566751794148016148 ||
                           eventArgs.Message.ChannelId == 633698411379556363)
-                    ))
+                    )) // check that the message was sent in hardcoded guilds, and make sure again that the message wasn't sent by the bot (to be really sure)
                 {
                     return;
                 } // TODO cleanup this sucks lol
@@ -68,11 +70,12 @@ namespace JazzMock.Services
             {
                 using (eventArgs.Channel.BeginTyping())
                 {
-                    var oldMessages = await eventArgs.Channel.FetchMessagesAsync(limit: 5, RetrievalDirection.Before,
-                        startFromId: eventArgs.MessageId, new DefaultRestRequestOptions());
+                    var oldMessages = await eventArgs.Channel.FetchMessagesAsync(limit: 4, RetrievalDirection.Before,
+                        startFromId: eventArgs.MessageId, new DefaultRestRequestOptions()); // get old messages for context
                     List<String> history = new List<string>();
                     foreach (var oldMessage in oldMessages)
                     {
+                        // trycatch to handle weird messages like pinned message indicators i can probably do something else
                         try
                         {
                             if (!String.IsNullOrWhiteSpace(oldMessage.Content))
@@ -98,7 +101,7 @@ namespace JazzMock.Services
                         genResponse = "_ _";
                     var msg = new LocalMessageBuilder()
                         .WithContent(genResponse);
-                    if (!fun) // since fun==1 responses are unprovoked "comments" in a conversation remove the reply
+                    if (!fun) // since fun responses are unprovoked "comments" in a conversation remove the reply
                         msg.WithReply(eventArgs.MessageId, eventArgs.ChannelId, eventArgs.GuildId);
                     Logger.LogInformation("replying...");
                     await _client.SendMessageAsync(eventArgs.ChannelId, msg.Build(), new DefaultRestRequestOptions());
@@ -110,15 +113,27 @@ namespace JazzMock.Services
             }
         }
 
-        public async Task<string> GenerateMessage(string prefixArg)
+        public async Task<string> GenerateMessage(string prefixArg, byte settings = 0b0000)
         {
+            _queue++;
+            if (_queue > 10)
+            {
+                throw new TaskCanceledException(); // obvious spam is obvious
+            }
             Logger.LogInformation("MessageReceived fired, generating response...");
             //if (_busySocket)
             //    _prompts.Push(prefixArg);
             //_busySocket = true;
-            _tunnel.SendFrame(prefixArg);
+            var message = new NetMQMessage();
+            message.Append(settings);
+            message.Append(prefixArg);
+            _tunnel.SendMultipartMessage(message);
             var response = _tunnel.ReceiveFrameString();
             Logger.LogInformation("received response...");
+            if (settings == 1) // genconvo?
+                response = response.Replace("<|startoftext|>", ">")
+                    .Replace("<|endoftext|>", "");
+            _queue--;
             //_busySocket = false;
             return response;
         }
