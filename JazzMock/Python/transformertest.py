a17134ab09e624aaea4c01e9dc6628e8a0e1f966
cielo <51804240+zielo-hue@users.noreply.github.com>
2021-05-09 19:38:44 -0700

    maybe i should try using huggingface stuff it looks neat

---
+++ JazzMock/Python/transformertest.py
@@ -0,0 +1,16 @@
+﻿from transformers import AutoTokenizer, TFAutoModelWithLMHead, pipeline, set_seed
+
+generator = pipeline('text-generation', model='distilgpt2')
+set_seed(42)
+generation = generator("I like transgender people because ", max_length=70, num_return_sequences=5)
+for thing in generation:
+    print(thing)
+
+# tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+
+#model = TFAutoModelWithLMHead.from_pretrained("distilgpt2")
+
+#text = "Some text"
+#encoded_input = tokenizer(text, return_tensors="tf")
+#output = model(encoded_input)
+#print(output)
